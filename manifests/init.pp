2d48656a8f3272d472bffe8be4a94a40b5f57876
Matthias Saou <matthias@saou.eu>
2014-05-13 09:39:15 +0200

    Start migration from a definition to a class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,6 @@
-# Define: apache_httpd
+# Class: apache_httpd
 #
-# Apache httpd web server main definition.
-#
-# The title must be 'prefork' (RHEL default) or 'worker'. The defaults are
-# as close as possible to the original RHEL defaults. The configuration files
-# are based on RHEL6's httpd 2.2 files, but work fine with RHEL5's 2.0 httpd.
+# Apache httpd web server main class.
 #
 # Note that by default a minimal set of modules is enabled in order to have
 # the original testing page work, but passing an array of module names as the
@@ -14,70 +10,65 @@
 #  Too many to list here. See the module's init.pp file.
 #
 # Sample Usage :
-#  apache_httpd { 'prefork':
-#      modules => [ 'mime' ],
+#  class { 'apache_httpd':
+#    modules => [ 'mime' ],
 #  }
 #
-define apache_httpd (
-    $ssl                    = false,
-    # Options for the sysconfig file
-    $options                = false,
-    $httpd_lang             = false,
-    $pidfile                = false,
-    # Options for httpd.conf
-    # These are not critical modules, but they are needed for welcome.conf
-    $modules                = [
-        'authz_host',
-        'include',
-        'mime',
-        'negotiation',
-        'alias',
-    ],
-    # The original default is 'OS', but 'Prod' makes more sense to hide version
-    $servertokens           = 'Prod',
-    # timeout is a reserved puppet variable
-    $timeout_seconds        = '60',
-    $keepalive              = 'Off',
-    $maxkeepaliverequests   = '100',
-    $keepalivetimeout       = '15',
-    $startservers           = '4',
-    $minspareservers        = '2',
-    $maxspareservers        = '8',
-    $serverlimit            = '256',
-    $maxclients             = '256',
-    $maxrequestsperchild    = '4000',
-    $minsparethreads        = '25',
-    $maxsparethreads        = '75',
-    $threadsperchild        = '32',
-    $listen                 = [ '80' ],
-    $namevirtualhost        = [],
-    $extendedstatus         = 'Off',
-    $user                   = 'apache',
-    $group                  = 'apache',
-    $serveradmin            = 'root@localhost',
-    $servername             = undef,
-    $usecanonicalname       = 'Off',
-    $documentroot           = '/var/www/html',
-    $serversignature        = 'On',
-    # Other
-    $welcome                = true,
-    $logrotate_files        = '/var/log/httpd/*log',
-    $logrotate_freq         = false,
-    $logrotate_opts         = [
-        'missingok',
-        'notifempty',
-        'sharedscripts',
-        'delaycompress',
-    ]
-) {
+class apache_httpd (
+  $worker                 = false,
+  $ssl                    = false,
+  # Options for the sysconfig file
+  $options                = false,
+  $httpd_lang             = false,
+  $pidfile                = false,
+  # Options for httpd.conf
+  # These are not critical modules, but they are needed for welcome.conf
+  $modules                = [
+    'authz_host',
+    'include',
+    'mime',
+    'negotiation',
+    'alias',
+  ],
+  # The original default is 'OS', but 'Prod' makes more sense to hide version
+  $servertokens           = 'Prod',
+  # timeout is a reserved puppet variable
+  $timeout_seconds        = '60',
+  $keepalive              = 'Off',
+  $maxkeepaliverequests   = '100',
+  $keepalivetimeout       = '15',
+  $startservers           = '4',
+  $minspareservers        = '2',
+  $maxspareservers        = '8',
+  $serverlimit            = '256',
+  $maxclients             = '256',
+  $maxrequestsperchild    = '4000',
+  $minsparethreads        = '25',
+  $maxsparethreads        = '75',
+  $threadsperchild        = '32',
+  $listen                 = [ '80' ],
+  $namevirtualhost        = [],
+  $extendedstatus         = 'Off',
+  $user                   = 'apache',
+  $group                  = 'apache',
+  $serveradmin            = 'root@localhost',
+  $servername             = undef,
+  $usecanonicalname       = 'Off',
+  $documentroot           = '/var/www/html',
+  $serversignature        = 'On',
+  # Other
+  $welcome                = true,
+  $logrotate_files        = '/var/log/httpd/*log',
+  $logrotate_freq         = false,
+  $logrotate_opts         = [
+    'missingok',
+    'notifempty',
+    'sharedscripts',
+    'delaycompress',
+  ],
+) inherits '::apache_httpd::params' {
 
-    # Since this is a definition, make the title somewhat meaningful
-    $worker = $title ? {
-        'worker'  => true,
-        'prefork' => false,
-    }
-
-    include apache_httpd::install
+  include '::apache_httpd::install'
 
     # Our own pre-configured file (disable nearly everything)
     file { '/etc/httpd/conf/httpd.conf':
---
+++ manifests/params.pp
@@ -0,0 +1,14 @@
+class apache_https::params {
+
+  case $::operatingsystem {
+    'RedHat','CentOS','Scientific': {
+      if $::operatingsystemrelease >= 7 {
+        $httpd_version = '2.4'
+      } else {
+        $httpd_version = '2.2'
+      }
+    },
+  }
+
+}
+
