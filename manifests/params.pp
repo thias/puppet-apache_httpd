d15df2e4d22d006f68a76d95541ebf1d57c59aee
Matthias Saou <matthias@saou.eu>
2014-05-13 10:25:01 +0200

    Move the service reload command to params and update logrotate template

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,9 +4,15 @@ class apache_httpd::params {
     'RedHat','CentOS','Scientific': {
       if $::operatingsystemrelease >= 7 {
         $httpd_version = '2.4'
+        $service_restart = '/bin/systemctl reload httpd.service'
       } else {
         $httpd_version = '2.2'
+        $service_restart = '/sbin/service httpd reload'
       }
+    }
+    default: {
+      $httpd_version = '2.2'
+      $service_restart = '/bin/kill -HUP `cat /var/run/httpd.pid 2>/dev/null`'
     }
   }
 
